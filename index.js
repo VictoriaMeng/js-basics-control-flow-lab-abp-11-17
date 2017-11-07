d1a40a86dabd6fa0e3ed52f7ddb1736297d58e41
Victoria Meng <nights1ststar@protonmail.com>
2017-11-07 01:51:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let greeting;
+
+  if (feet < 400) {
+    greeting = 'This one is on me!';
+  } else if (feet > 2500) {
+    greeting = 'No can do.';
+  } else if (feet > 2000) {
+    greeting = 'I will gladly take your thirty bucks.';
+  }
+
+  return greeting
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip() {
+  
+}
